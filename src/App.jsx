f40748f1b97ebcd87cb4c2b2578aa7745cc7e6eb
toaa <toaa111@gmail.com>
2026-08-09 14:16:18 +0300

    Add chatAI page with empty state, conversation and recipe detail

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { BrowserRouter } from "react-router-dom";
-import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home/Home";
+import Explore from "./pages/Explore/Explore";
+import ChatAI from "./pages/ChatAI/ChatAI";
+import RecipeDetail from "./pages/RecipeDetail/RecipeDetail";
+import Sidebar from "./components/layout/Sidebar/Sidebar";
+import TopHeader from "./components/layout/TopHeader/TopHeader";
 
 function App() {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <div className="bg-main-bg min-h-screen">
+        <TopHeader />
+        <Sidebar />
+
+        <div className="lg:pl-[88px] lg:pt-[77px] bg-main-bg min-h-screen">
+          <div className="lg:px-12">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/explore" element={<Explore />} />
+              <Route path="/chat-ai" element={<ChatAI />} />
+              <Route path="/recipe/:id" element={<RecipeDetail />} />
+            </Routes>
+          </div>
+        </div>
+      </div>
     </BrowserRouter>
   );
 }
